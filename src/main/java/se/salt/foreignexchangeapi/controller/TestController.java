7a9e0fe5719409dd2d0efe999f71f22181dd32e7
sh1re-a <Shirwac.abib@gmail.com>
2026-03-02 12:26:57 +0100

    Nice, now we have converted to JAVA Object, and through Hashmap im storing the rates and I should probably refactor some names, look kinda messy. But DTO is working :)

--- src/main/java/se/salt/foreignexchangeapi/controller/TestController.java
+++ src/main/java/se/salt/foreignexchangeapi/controller/TestController.java
@@ -3,6 +3,8 @@ package se.salt.foreignexchangeapi.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.salt.foreignexchangeapi.client.ApiClient;
+import se.salt.foreignexchangeapi.dto.FrankfurterLatestResponse;
+
 @RestController
 public class TestController {
     private final ApiClient apiClient;
@@ -12,7 +14,8 @@ public class TestController {
     }
 
     @GetMapping("/test")
-    public String test() {
-        return apiClient.convertCurrency("EUR", "SEK");
+    public double test() {
+        FrankfurterLatestResponse frankfurterLatestResponse = apiClient.convertCurrency("EUR", "SEK");
+        return frankfurterLatestResponse.rates().get("SEK");
     }
 }
--- src/main/java/se/salt/foreignexchangeapi/dto/FrankfurterLatestResponse.java
+++ src/main/java/se/salt/foreignexchangeapi/dto/FrankfurterLatestResponse.java
@@ -3,7 +3,7 @@ package se.salt.foreignexchangeapi.dto;
 import java.util.Map;
 
 public record FrankfurterLatestResponse(
-        double Amount,
+        Double Amount,
         String base,
         String date,
         Map<String, Double> rates) { }
